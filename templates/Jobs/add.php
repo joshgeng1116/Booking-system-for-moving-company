73951ce0d921307cbd4cc244a430647b27295e3f
dazza <dsac0001@student.monash.edu>
2021-09-06 13:10:43 +1000

    Darren finished refactoring Jobs/Add.php so that all html inputs are CakePHP generated. This allows the sending of a Post request to the relevent controller. Now must extract the data given by Post request and update database.

--- templates/Jobs/add.php
+++ templates/Jobs/add.php
@@ -10,40 +10,6 @@ $this->disableAutoLayout();
 
 ?>
 
-
-
-<!--<div class="row">-->
-<!--    <aside class="column">-->
-<!--        <div class="side-nav">-->
-<!--            <h4 class="heading">--><?//= __('Actions') ?><!--</h4>-->
-<!--            --><?//= $this->Html->link(__('List Jobs'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-<!--        </div>-->
-<!--    </aside>-->
-<!--    <div class="column-responsive column-80">-->
-<!--        <div class="jobs form content">-->
-<!--            --><?//= $this->Form->create($job) ?>
-<!--            <fieldset>-->
-<!--                <legend>--><?//= __('Add Job') ?><!--</legend>-->
-<!--                --><?php
-//                    echo $this->Form->control('customer_id', ['options' => $customers]);
-//                    echo $this->Form->control('allocation_id', ['options' => $allocation]);
-//                    echo $this->Form->control('status');
-//                    echo $this->Form->control('moving_from');
-//                    echo $this->Form->control('moving_to');
-//                    echo $this->Form->control('list_of_item');
-//                    echo $this->Form->control('size');
-//                    echo $this->Form->control('date');
-//                    echo $this->Form->control('deposit_status');
-//                    echo $this->Form->control('total_paid');
-//                    echo $this->Form->control('total_remaining');
-//                ?>
-<!--            </fieldset>-->
-<!--            --><?//= $this->Form->button(__('Submit')) ?>
-<!--            --><?//= $this->Form->end() ?>
-<!--        </div>-->
-<!--    </div>-->
-<!--</div>-->
-
 <!-- Darren custom HTML -->
 <!doctype html>
 <html lang="en">
@@ -127,8 +93,10 @@ $this->disableAutoLayout();
 
 <!-- landing page content -->
 <div class="container w-50">
-    <form actions="">
-        <?= $this->Form->create($job) ?>
+
+        <?php
+        echo $this->Form->create($job);
+        ?>
         <div class="contact_section proceed form_section">
 
             <h2 class="text-center text-secondary">You...</h2>
@@ -136,11 +104,14 @@ $this->disableAutoLayout();
             <div class="row">
                 <div class="form-group col-md-6">
                     <label for="first_name" class="mandatory" id="first_name_label">First Name</label>
-                    <input maxlength="100" type="text" name="first_name" value="" class="first_name form-control" required>
+                    <?=$this->Form->input('first_name', ["class" => "first_name form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "maxlength" => "100", "required", "name" => "first_name", "type" => "text", "value" => ""])?>
+
                 </div>
                 <div class="form-group col-md-6">
                     <label for="last_name" class="mandatory" id="last_name_label">Last Name</label>
-                    <input maxlength="100" type="text" name="last_name" value="" class="last_name form-control" required>
+                    <!--<input maxlength="100" type="text" name="last_name" value="" class="last_name form-control" required>-->
+                    <?=$this->Form->input('first_name', ["class" => "last_name form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "maxlength" => "100", "required", "name" => "last_name_name", "type" => "text", "value" => ""])?>
+
                 </div>
             </div>
 
@@ -148,16 +119,21 @@ $this->disableAutoLayout();
 
                 <div class="form-group col-md-6">
                     <label for="phone_number" class="mandatory" id="phone_label">Phone Number</label>
-                    <input type="tel" name="phone_number" minlength="8" maxlength="10" value="" class="phone_number form-control" required>
+                    <!--<input type="tel" name="phone_number" minlength="8" maxlength="10" value="" class="phone_number form-control" required>-->
+                    <?=$this->Form->input('phone_number', ["class" => "phone_number form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "minlength"=>"8", "maxlength" => "10", "required", "name" => "phone_number", "type" => "tel", "value" => ""])?>
+
                 </div>
                 <div class="form-group col-md-6">
                     <label for="email" aria-describedby="emailHelp" class="mandatory" class="mandatory" id="email_label">Email Address</label>
-                    <input maxlength="100" type="email" name="email" value="" class="email form-control" required>
+                    <!--<input maxlength="100" type="email" name="email" value="" class="email form-control" required>-->
+                    <?=$this->Form->input('email', ["class" => "email form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "maxlength" => "100", "required", "name" => "email", "type" => "tel", "value" => ""])?>
+
                 </div>
 
             </div>
 
         </div>
+
 
         <div class="job_section no-proceed form_section">
 
@@ -166,17 +142,22 @@ $this->disableAutoLayout();
             <div class="row">
                 <div class="form-group col-md-6">
                     <label for="move_from" class="mandatory" id="from_label">Moving From</label>
-                    <input maxlength="400" type="text" name="moving_from" value="" class="moving_from form-control" required>
+                    <!--<input maxlength="400" type="text" name="moving_from" value="" class="moving_from form-control" required>-->
+                    <?=$this->Form->input('moving_from', ["class" => "moving_from form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "maxlength" => "400", "required", "name" => "moving_from", "type" => "text", "value" => ""])?>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="move_to" class="mandatory" id="to_label">Moving To</label>
-                    <input maxlength="400" type="text" name="moving_to" value="" class="moving_to form-control" required>
+                    <!---->
+                    <?=$this->Form->input('moving_to', ["class" => "moving_to form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "maxlength" => "400", "required", "name" => "moving_to", "type" => "text", "value" => ""])?>
+
                 </div>
 
             </div>
             <div class="form-group">
                 <label class="mandatory" for="items_list" id="items_list_label">List of Items</label>
-                <textarea maxlength="600" type="text" name="items_list" value="" class="items_list form-control" required></textarea>
+                <!--<textarea maxlength="600" type="text" name="items_list" value="" class="items_list form-control" required></textarea>-->
+                <?=$this->Form->input('items_list', ["class" => "items_list form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "maxlength" => "600", "required", "name" => "items_list", "type" => "textarea", "value" => ""])?>
+
             </div>
 
 
@@ -188,6 +169,13 @@ $this->disableAutoLayout();
             <h2 class="text-center text-secondary" id="truck_size_label">The Truck...</h2>
 
             <div class="form-group row justify-content-center">
+                <?php
+                $options = ['none_chosen' => 'Choose...', '2T' => 'Just a few items', '4T' => '1-2 bedrooms / small office', '8T' => '3-4 bedrooms / medium office', '10T' =>  '4-5 bedrooms / medium office', '12' => '4-5 bedrooms / large office'];
+                echo $this->Form->select('truck_size', $options, ['choose' => true, "class" => 'mandatory truck_size form-control', 'required']);
+
+                ?>
+
+                <!--
                 <select class="mandatory truck_size form-control" required>
                     <option value="none_chosen" selected>Choose...</option>
                     <option value="2T">Just a few items</option>
@@ -196,6 +184,7 @@ $this->disableAutoLayout();
                     <option value="10T">4-5 bedrooms / medium office</option>
                     <option value="12T">4-5 bedrooms / large office</option>
                 </select>
+                -->
             </div>
 
 
@@ -218,15 +207,18 @@ $this->disableAutoLayout();
             <div class="row">
                 <div class="form-group col-md-4">
                     <label for="start_date" class="mandatory" id="start_label">Start Date</label>
-                    <input type="date" name="start_date" value="" class="start_date form-control" required />
+                    <!--<input type="date" name="start_date" value="" class="start_date form-control" required />-->
+                    <?=$this->Form->input('start_date', ["class" => "start_date form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "required", "name" => "start_date", "type" => "date", "value" => ""])?>
                 </div>
                 <div class="form-group col-md-4">
                     <label for="start_time" class="mandatory" id="start_time_label">Start Time</label>
-                    <input type="time" name="start_time" value="" class="start_time form-control" required />
+                    <!--<input type="time" name="start_time" value="" class="start_time form-control" required />-->
+                    <?=$this->Form->input('start_time', ["class" => "start_time form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "required", "name" => "start_time", "type" => "time", "value" => ""])?>
                 </div>
                 <div class="form-group col-md-4">
                     <label for="end_time" class="mandatory" id="end_time_label">End Time</label>
-                    <input type="time" name="end_time" value="" class="end_time form-control" required />
+                    <!--<input type="time" name="end_time" value="" class="end_time form-control" required />-->
+                    <?=$this->Form->input('end_time', ["class" => "end_time form-control", "oninput" => "validate(); updateReview(); unlockNextSection();", "required", "name" => "end_time", "type" => "time", "value" => ""])?>
                 </div>
             </div>
 
@@ -237,15 +229,13 @@ $this->disableAutoLayout();
 <div class="no-proceed note_section form_section w-50 mx-auto">
     <div class="form-group">
         <h2 class="text-center text-secondary">Anything Else?</h2>
-        <textarea maxlength="500" type="textarea" name="special_note" width="100" class="special_note_input form-control" value="" placeholder="Want us to know something specific? Optional."></textarea>
+        <!--<textarea maxlength="500" type="textarea" name="special_note" width="100" class="special_note_input form-control" value="" placeholder="Want us to know something specific? Optional."></textarea>-->
+        <?=$this->Form->input('special_note', ["class" => "special_note_input form-control", "name" => "special_note", "type" => "textarea", "value" => "", "placeholder" => "Want us to know something specific? Optional."])?>
+
     </div>
 </div>
 
 
-
-
-</form>
-</div>
 
 
 
@@ -281,13 +271,15 @@ $this->disableAutoLayout();
 
 </div>
 
-<div class="container no-proceed submit_section form_section">
+
+
+<!-- todo uncomment <div class="container no-proceed submit_section form_section"> -->
+    <div class="container submit_section form_section">
 
     <div class="form-group">
         <h2 class="text-center text-secondary">Then Let's Talk!</h2>
         <div class="center">
             <?= $this->Form->button(__('Submit'), ['class' => "btn btn-success submit-btn"]) ?>
-            <?= $this->Form->end() ?>
         </div>
     </div>
 
@@ -315,6 +307,9 @@ $this->disableAutoLayout();
 
 </div>
 
+<?= $this->Form->end() ?>
+
+
 
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -326,6 +321,7 @@ $this->disableAutoLayout();
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 <!-- original JS -->
+
 <?php
 echo $this->Html->script('add_enquiry_logic');
 ?>
