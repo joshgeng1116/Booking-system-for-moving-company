4dcf0451bcffb6d7062c1c41a53601d4c803d397
dazza <dsac0001@student.monash.edu>
2021-10-14 13:14:48 +1100

    Fixed phone number validation as we changed it to a VARCHAR in database. Should work properly now.

--- src/Model/Table/JobsTable.php
+++ src/Model/Table/JobsTable.php
@@ -76,7 +76,7 @@ class JobsTable extends Table
 
 
         $validator
-            ->integer('customer_phone')
+            ->scalar('customer_phone')
             ->requirePresence('customer_phone', 'create')
             ->notEmptyString('customer_phone')
             ->regex('customer_phone', '/^(0|\+?61)\d{9}$/', 'Phone number must begin with 0, 61 or +61. Then enter remaining nine digits immmediately.');
